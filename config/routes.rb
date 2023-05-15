e6413e4405d6873d6f3a04cd8bc4e04764f483b5
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-05-15 14:39:21 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   post 'books' => 'books#create'
   get 'books' => 'books#index'
   get 'top' => 'homes#top'
-  get 'books/:id' => 'books#show'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
 end
