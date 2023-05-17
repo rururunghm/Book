9021d52d57471c26f8883b0ac9ee073e652c0dba
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-05-17 20:12:54 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,19 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
   
   def create
      @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(@book.id)
+        redirect_to book_path(@book.id)
     else
-+     @book = Book.all    
+      @books = Book.all.order(id: :asc) 
       render :index 
       flash[:notice] = "Book was successfully created."
     end
   end
 
   def index
-    @books = Book.all 
+    @books = Book.all.order(id: :asc)
+    @book = Book.new
   end
 
   def show
@@ -27,19 +25,19 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
+    @book.update(book_params)
     redirect_to book_path(book.id)  
   end
   
   def destroy
-    book = Book.find(params[:id]) 
-    book.destroy 
+    @book = Book.find(params[:id]) 
+    @book.destroy 
     redirect_to '/books' 
   end
   
    private
     def book_params
     params.require(:book).permit(:title, :body)
-  end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'books/new'
+  get 'top' => 'homes#top'
   resources :books
 end
